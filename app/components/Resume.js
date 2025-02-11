7d9c97beb0fc778a9782ec861db425d27c500d3d
UdayGattu <udaygattu007@gmail.com>
2025-02-11 00:33:49 -0500

    done

--- app/components/Resume.js
+++ app/components/Resume.js
@@ -6,9 +6,9 @@ export default function Resume() {
   return (
     <section id="cv" className="py-12 bg-dark">
       <h2 className="text-3xl font-bold text-center mb-4 text-accent">
-        Resume{" "}
+      Curriculum Vitae (CV){" "}
         <a 
-          href="/cv.pdf" 
+          href="/CV.pdf" 
           download 
           className="inline-block ml-2 hover:text-red-400"
         >
@@ -20,7 +20,7 @@ export default function Resume() {
           initial={{ opacity: 0, scale: 0.95 }}
           animate={{ opacity: 1, scale: 1 }}
           transition={{ duration: 1 }}
-          src="/cv.pdf"  // Replace with your actual resume PDF when available
+          src="/CV.pdf"  // Replace with your actual resume PDF when available
           width="80%"
           height="600px"
           className="border-2 border-accent rounded-lg shadow-lg"
